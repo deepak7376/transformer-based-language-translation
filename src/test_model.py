97e065b49e66d5a07a725b6b09c296c6c5315d54
DeepakYadav <41716815+deepak7376@users.noreply.github.com>
2023-09-15 10:18:29 +0000

    modify the test-model

--- src/test_model.py
+++ src/test_model.py
@@ -1,12 +1,27 @@
 import torch
 from model import TransformerModel  # Import your Transformer model implementation
 
+# Initialize your model and other necessary configurations
+# Check if a GPU is available and use it, otherwise use the CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+input_vocab_size = 1000
+target_vocab_size = 1500
+input_seq_length = 120
+target_seq_length = 120
+d_model = 512
+nhead = 8
+num_encoder_layers = 2
+num_decoder_layers = 2
+dim_feedforward = 2048
+dropout = 0.1
+
 def test_model_with_random_data(model, device):
     # Generate random input tensor (English)
-    input_tensor = torch.randint(0, input_vocab_size, (1, input_seq_length), dtype=torch.long, device=device)
+    
+    input_tensor = torch.randint(0, input_vocab_size, (10, input_seq_length), dtype=torch.long, device=device)
 
     # Generate random target input tensor (Hindi)
-    target_input_tensor = torch.randint(0, target_vocab_size, (1, target_seq_length), dtype=torch.long, device=device)
+    target_input_tensor = torch.randint(0, target_vocab_size, (10, target_seq_length), dtype=torch.long, device=device)
 
     # Pass the input tensor through the model for translation
     output = model(input_tensor, target_input_tensor)
@@ -17,15 +32,20 @@ def test_model_with_random_data(model, device):
     return output
 
 def main():
-    # Initialize your model and other necessary configurations
     model = TransformerModel(input_vocab_size, target_vocab_size, d_model, nhead, num_encoder_layers, num_decoder_layers, dim_feedforward, dropout)
     model.to(device)
+    print("Device:", device)
+
+    # You can also get more information about the GPU device, if available
+    if device.type == "cuda":
+        print("GPU Name:", torch.cuda.get_device_name(0))
+        print("GPU Memory:", torch.cuda.max_memory_allocated(0) / (1024 * 1024), "MB")
     
     # Call the test function with random data
     output = test_model_with_random_data(model, device)
 
     # Print or process the model output as needed
-    print(output)
+    print(output.shape)
 
 if __name__ == '__main__':
     main()
